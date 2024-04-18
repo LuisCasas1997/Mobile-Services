50dc979b69994eee5cc7c6ca695e3921d36e8080
Luis Casas <jfabila@jexbit.mx>
2024-04-18 16:18:54 -0600

    InitialCommint

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyAz4rnNFBQXFJ77RljawbDxj0RcTuiHLx8",
+      authDomain: "mobile-services-3541b.firebaseapp.com",
+      projectId: "mobile-services-3541b",
+      storageBucket: "mobile-services-3541b.appspot.com",
+      messagingSenderId: "271961961900",
+      appId: "1:271961961900:web:7aad4d321c3e2534c23609"
+    }
+  }
